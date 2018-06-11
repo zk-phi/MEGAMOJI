164e5c55ae25ad2c0ef4c1385ca3d4fafa502bc3
p <_>
2018-06-11 12:03:38 +0900

    Add feature to create a kira card

--- megamoji.js
+++ megamoji.js
@@ -19,6 +19,32 @@ function update_default_configuration () {
     $("#zoom").val(zoom_ratio + "");
     $("#left").val((image.naturalWidth - cell_size * h) / 2 + "");
     $("#top").val(0);
+    $("#top").removeProp("checked");
+}
+
+function render_cell (image, trimLeft, trimRight, width, height, kira) {
+    var canvas = document.createElement("canvas");
+    canvas.width = 128;
+    canvas.height = 128;
+
+    var ctx = canvas.getContext('2d');
+
+    if (!kira) {
+        ctx.drawImage(image, trimLeft, trimRight, width, height, 0, 0, 128, 128);
+        return canvas.toDataURL();
+    } else {
+        var encoder = new GIFEncoder();
+        encoder.setRepeat(0);
+        encoder.setDelay(1000 / 30); /* 30 = FPS */
+        encoder.start();
+        for (var i = 0; i < 30; i++) {
+            ctx.filter = "saturate(1000%) hue-rotate(" + (i * 12) + "deg)";
+            ctx.drawImage(image, trimLeft, trimRight, width, height, 0, 0, 128, 128);
+            encoder.addFrame(ctx);
+        }
+        encoder.finish();
+        return "data:image/gif;base64," + encode64(encoder.stream().getData());
+    }
 }
 
 function render_result () {
@@ -28,20 +54,17 @@ function render_result () {
     var zoom_ratio = parseFloat($("#zoom").val());
     var left       = parseInt($("#left").val());
     var top        = parseInt($("#top").val());
+    var kirality   = $("#kira").prop("checked");
 
     var cell_size = 128 * zoom_ratio;
     var $result_area = $("#result-area");
     $result_area.html("");
     for (var y = 0; y < v; y++) {
         for (var x = 0; x < h; x++) {
-            var $canvas = $("<canvas width=128 height=128>").appendTo($result_area);
-            var ctx = $canvas[0].getContext('2d');
-            ctx.drawImage(
-                image,
-                left + x * cell_size, top + y * cell_size, cell_size, cell_size,
-                0, 0, 128, 128
+            var url = render_cell(
+                image, left + x * cell_size, top + y * cell_size, cell_size, cell_size, kirality
             );
-            $result_area.append("&nbsp;");
+            $result_area.append("<img src='" + url +"'>&nbsp;");
         }
         $result_area.append("<br>");
     }
