d2d6a9964d2429339346d107c957e1e4a6f22619
zk-phi <zk-phi@users.noreply.github.com>
2020-10-16 20:14:23 +0900

    Remove duplicate pageview

--- js/ga.js
+++ js/ga.js
@@ -4,4 +4,3 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
 ga('create', 'UA-121793995-1', 'auto');
-ga('send', 'pageview');
