c7c5ac8cef53cb062007676559569a53f187df3f
zk-phi <zk-phi@users.noreply.github.com>
2021-02-09 19:19:51 +0900

    Enable airbnb-base eslint rules (with some exceptions)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    browser: true
+    browser: true,
   },
   globals: {
     Vue: true,
@@ -8,5 +8,74 @@ module.exports = {
     encode64: true,
     ga: true,
   },
-  extends: "eslint:recommended"
-}
+  extends: 'airbnb-base',
+  rules: {
+
+    // redundant "else" can help readablity
+    'no-else-return': 'off',
+
+    // ternary ops aren't that unreadable IMO
+    'no-nested-ternary': 'off',
+
+    // bitwise ops are sometimes useful
+    'no-bitwise': 'off',
+
+    // "continue" can help readablity as like "early-return"
+    'no-continue': 'off',
+
+    // fns and classes can be referred before defined
+    'no-use-before-define': ['error', {
+      // functions: true,
+      // classes: true,
+      functions: false,
+      classes: false,
+      variables: true,
+    }],
+
+    // do not enforce destructuring for arrays (since they can be rather unreadable sometime)
+    'prefer-destructuring': ['error', {
+      VariableDeclarator: {
+        array: false,
+        object: true,
+      },
+      AssignmentExpression: {
+        // array: true,
+        array: false,
+        object: false,
+      },
+    }, {
+      enforceForRenamedProperties: false,
+    }],
+
+    // arithmetic ops (eccept for "%" and "**") can be mixed without parens
+    'no-mixed-operators': ['error', {
+      groups: [
+        ['%', '**'],
+        // ['%', '+'],
+        // ['%', '-'],
+        ['%', '*'],
+        ['%', '/'],
+        // ['/', '*'],
+        ['&', '|', '<<', '>>', '>>>'],
+        ['==', '!=', '===', '!=='],
+        ['&&', '||'],
+      ],
+      allowSamePrecedence: false,
+    }],
+
+    // labels are allowed to break nested loops
+    'no-restricted-syntax': [
+      'error',
+      'ForInStatement',
+      'ForOfStatement',
+      // 'LabeledStatement',
+      'WithStatement',
+    ],
+    'no-labels': ['error', {
+      // allowLoop: false,
+      allowLoop: true,
+      allowSwitch: false,
+    }],
+
+  },
+};
