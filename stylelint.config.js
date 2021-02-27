613717aa1cb683a2e79aaf268539c4378a37d9d4
zk-phi <zk-phi@users.noreply.github.com>
2021-02-27 18:21:56 +0900

    Fix linter issues

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-    "extends": "stylelint-config-primer",
-    "rules": {
+    extends: "stylelint-config-primer",
+    rules: {
         // stylelint-config-primer dropped these rules from v11
         indentation: 2,
         "string-quotes": "double",
         "value-list-comma-newline-after": "always-multi-line",
         // allow css-gradients, which is not allowed by default
         "plugin/no-unsupported-browser-features": [true, {
-            "severity": "error",
-            "ignore": ["css-gradients"],
+            severity: "error",
+            ignore: ["css-gradients"],
         }],
-    }
-}
+    },
+};
