d60f4fa88be9c070482b3228cc4d5c9d6f3d77ca
zk-phi <zk-phi@users.noreply.github.com>
2021-04-26 00:34:42 +0900

    Mosaic: Better alpha calculation

--- js/posteffects.js
+++ js/posteffects.js
@@ -79,31 +79,30 @@ function postEffectMosaic(keyframe, ctx, w, h) {
 
   for (let cellX = 0; cellX < cellCountH; cellX += 1) {
     for (let cellY = 0; cellY < cellCountV; cellY += 1) {
-      const cellColor = [0, 0, 0];
-      let count = 0;
+      const cellColor = [0, 0, 0, 0];
       const x = offsetX + cellX * cellSize;
       const y = offsetY + cellY * cellSize;
       for (let dx = 0; dx < cellSize; dx += 1) {
         for (let dy = 0; dy < cellSize; dy += 1) {
           const ix = (y + dy) * w + x + dx;
-          const alpha = data[ix * 4 + 3] / 255;
-          cellColor[0] += alpha * data[ix * 4 + 0] + (1 - alpha) * 255;
-          cellColor[1] += alpha * data[ix * 4 + 1] + (1 - alpha) * 255;
-          cellColor[2] += alpha * data[ix * 4 + 2] + (1 - alpha) * 255;
-          count += alpha > 0 ? 1 : 0;
+          cellColor[0] += data[ix * 4 + 0] * data[ix * 4 + 3];
+          cellColor[1] += data[ix * 4 + 1] * data[ix * 4 + 3];
+          cellColor[2] += data[ix * 4 + 2] * data[ix * 4 + 3];
+          cellColor[3] += data[ix * 4 + 3];
         }
       }
 
-      cellColor[0] /= cellSize * cellSize;
-      cellColor[1] /= cellSize * cellSize;
-      cellColor[2] /= cellSize * cellSize;
+      cellColor[0] /= cellColor[3];
+      cellColor[1] /= cellColor[3];
+      cellColor[2] /= cellColor[3];
+      cellColor[3] /= cellSize * cellSize;
       for (let dx = 0; dx < cellSize; dx += 1) {
         for (let dy = 0; dy < cellSize; dy += 1) {
           const ix = (y + dy) * w + x + dx;
           data[ix * 4 + 0] = cellColor[0];
           data[ix * 4 + 1] = cellColor[1];
           data[ix * 4 + 2] = cellColor[2];
-          data[ix * 4 + 3] = count ? 255 : 0;
+          data[ix * 4 + 3] = cellColor[3];
         }
       }
     }
