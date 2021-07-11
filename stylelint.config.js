8272ee7bf8117bcff3415bbc98604cb17ec17ebd
zk-phi <zk-phi@users.noreply.github.com>
2021-07-11 21:52:47 +0900

    Update linter config

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,10 +1,17 @@
 module.exports = {
   extends: "stylelint-config-primer",
   rules: {
+
     // stylelint-config-primer dropped these rules from v11
     indentation: 2,
+
     "string-quotes": "double",
+
     "value-list-comma-newline-after": "always-multi-line",
+
+    // do not require newlines for inline styles
+    "declaration-block-semicolon-newline-after": null,
+
     // allow css-gradients, which is not allowed by default
     "plugin/no-unsupported-browser-features": [true, {
       severity: "error",
