6587e27755f75e3e55bd47f2b4b4989b84c68cf4
zk-phi <zk-phi@users.noreply.github.com>
2021-07-12 16:26:04 +0900

    Fix debugrollbar command

--- src/megamoji.ts
+++ src/megamoji.ts
@@ -17,11 +17,6 @@ window.onerror = (msg, file, line, col) => {
   ga("send", "event", "error", "thrown", `${file}:${line}:${col} ${msg}`);
 };
 
-const match = window.location.href.match(/debugrollbar=(.*)/);
-if (match) {
-  throw new Error(match[1]);
-}
-
 app.mount("#app");
 
 ga("send", "pageview", "/");
