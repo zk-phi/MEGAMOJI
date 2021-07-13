6f8e19dfd5f0baf9b621c490a75180cbae14f1ed
zk-phi <zk-phi@users.noreply.github.com>
2021-07-13 16:55:23 +0900

    Refactor colors.el

--- utils/colors.el
+++ utils/colors.el
@@ -1,7 +1,13 @@
 (require 'color)
 
-(mapcar (lambda (l)
-          (mapcar (lambda (h)
-                    (apply 'color-rgb-to-hex (nconc (color-hsl-to-rgb (* h (/ 1 7.0)) 1 l) '(2))))
-                  '(0 1 2 3 4 5 6)))
-        '(0.3 0.5 0.7))
+(defun iota (from to)
+  (and (< from to) (cons from (iota (1+ from) to))))
+
+(let ((hues 7)
+      (lums '(0.3 0.5 0.7)))
+  (mapcar (lambda (l)
+            (mapcar (lambda (h)
+                      (apply 'color-rgb-to-hex
+                             (nconc (color-hsl-to-rgb (* h (/ 1.0 hues)) 1 l) '(2))))
+                    (iota 0 hues)))
+          lums))
