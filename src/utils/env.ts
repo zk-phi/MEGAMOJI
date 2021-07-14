6572c3f29bd1e5783d00e405465d8f13ce2e8067
zk-phi <zk-phi@users.noreply.github.com>
2021-07-15 03:30:38 +0900

    Annotate environment vars

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,5 +1,5 @@
 // eslint-disable-next-line prefer-destructuring
-export const ROLLBAR_TOKEN = process.env.ROLLBAR_TOKEN;
+export const ROLLBAR_TOKEN: (string | undefined) = process.env.ROLLBAR_TOKEN;
 
 // eslint-disable-next-line prefer-destructuring
-export const NODE_ENV = process.env.NODE_ENV;
+export const NODE_ENV: (string | undefined) = process.env.NODE_ENV;
