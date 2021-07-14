cde18598c43d9569e4840a58727e9bc2a95788f9
zk-phi <zk-phi@users.noreply.github.com>
2021-07-14 23:51:16 +0900

    Fix call signature for convert-color

--- src/utils/color.ts
+++ src/utils/color.ts
@@ -4,19 +4,19 @@ export type HSV = { h: number, s: number, v: number };
 export type RGB = { r: number, g: number, b: number };
 
 export const HSV2RGB = (hsv: HSV): RGB => {
-  const ret = ColorConvert.hsv.rgb(hsv.h, hsv.s * 100, hsv.v * 100);
+  const ret = ColorConvert.hsv.rgb([hsv.h, hsv.s * 100, hsv.v * 100]);
   return { r: ret[0], g: ret[1], b: ret[2] };
 };
 
 const lighterColor = (hexColor: string): string => {
   const hsl = ColorConvert.hex.hsl(hexColor);
-  const newColor = ColorConvert.hsl.hex(hsl[0], hsl[1], Math.min(100, hsl[2] + 20));
+  const newColor = ColorConvert.hsl.hex([hsl[0], hsl[1], Math.min(100, hsl[2] + 20)]);
   return `#${newColor}`;
 };
 
 const darkerColor = (hexColor: string): string => {
   const hsl = ColorConvert.hex.hsl(hexColor);
-  const newColor = ColorConvert.hsl.hex(hsl[0], hsl[1], Math.max(0, hsl[2] - 15));
+  const newColor = ColorConvert.hsl.hex([hsl[0], hsl[1], Math.max(0, hsl[2] - 15)]);
   return `#${newColor}`;
 };
 
