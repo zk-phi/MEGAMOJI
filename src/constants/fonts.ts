d339506f8b7313807aa697c8c9619d8b5f7284be
zk-phi <zk-phi@users.noreply.github.com>
2021-07-15 17:44:03 +0900

    Load fonts with asset modules

--- src/constants/fonts.ts
+++ src/constants/fonts.ts
@@ -1,3 +1,27 @@
+import GenEiMGothicBold from "../fonts/GenEiMGothic2-Bold.woff";
+import GenEiMGothicBlack from "../fonts/GenEiMGothic2-Black.woff";
+import RoundedMPlusBold from "../fonts/m-plus-rounded-1c-v10-latin_japanese-700.woff";
+import RoundedMPlusBlack from "../fonts/m-plus-rounded-1c-v10-latin_japanese-900.woff";
+import NotoSerifBlack from "../fonts/noto-serif-jp-v7-latin_japanese-900.woff";
+import DelaGothic from "../fonts/DelaGothicOne-Regular.woff";
+import AkazukinPOP from "../fonts/AkazukiPOP.woff";
+import ZeroGothic from "../fonts/ZeroGothic.woff";
+import KurobaraCinderella from "../fonts/kurobara-cinderella.woff";
+import AoyagiReishoSIMO from "../fonts/aoyagireisyosimo_ttf_2_01.woff";
+import PixelMplus from "../fonts/PixelMplus12-Bold.woff";
+import Reggae from "../fonts/ReggaeOne-Regular.woff";
+import Rampart from "../fonts/RampartOne-Regular.woff";
+
+const font = (font: Record<string, string>) => {
+  const family = Object.keys(font)[0];
+  const url = font[family];
+  const fontFace = new FontFace(family, `url('${url}')`);
+  fontFace.load().then(() => {
+    document.fonts.add(fontFace);
+  });
+  return `normal 1em '${family}'`;
+};
+
 export default [
   {
     label: "システムフォント",
@@ -8,23 +32,23 @@ export default [
   }, {
     label: "スタンダード",
     fonts: [
-      { label: "ゴシック (太)", value: "bold 1em 'GenEiMGothic'" },
-      { label: "ゴシック (極太)", value: "900 1em 'GenEiMGothic'" },
-      { label: "丸ゴ (太)", value: "bold 1em 'M PLUS Rounded 1c'" },
-      { label: "丸ゴ (極太)", value: "900 1em 'M PLUS Rounded 1c'" },
-      { label: "明朝 (太)", value: "900 1em 'Noto Serif JP'" },
+      { label: "ゴシック/太", value: font({ GenEiMGothicBold }) },
+      { label: "ゴシック/極太", value: font({ GenEiMGothicBlack }) },
+      { label: "丸ゴ/太", value: font({ RoundedMPlusBold }) },
+      { label: "丸ゴ/極太", value: font({ RoundedMPlusBlack }) },
+      { label: "明朝/太", value: font({ NotoSerifBlack }) },
     ],
   }, {
     label: "デザイン",
     fonts: [
-      { label: "Dela Gothic One", value: "normal 1em 'DelaGothicOne-Regular'" },
-      { label: "あかずきんポップ", value: "normal 1em 'AkazukiPOP'" },
-      { label: "零ゴシック", value: "normal 1em 'ZeroGothic'" },
-      { label: "黒薔薇シンデレラ", value: "normal 1em 'kurobara-cinderella'" },
-      { label: "青柳隷書しも", value: "normal 1em 'AoyagiReishoSIMO'" },
-      { label: "PixelMplus, bold", value: "normal 1em 'PixelMplus'" },
-      { label: "レゲエ", value: "normal 1em 'Reggae'" },
-      { label: "ランパート", value: "normal 1em 'Rampart'" },
+      { label: "Dela Gothic One", value: font({ DelaGothic }) },
+      { label: "あかずきんポップ", value: font({ AkazukinPOP }) },
+      { label: "零ゴシック", value: font({ ZeroGothic }) },
+      { label: "黒薔薇シンデレラ", value: font({ KurobaraCinderella }) },
+      { label: "青柳隷書しも", value: font({ AoyagiReishoSIMO }) },
+      { label: "PixelMplus", value: font({ PixelMplus }) },
+      { label: "レゲエ", value: font({ Reggae }) },
+      { label: "ランパート", value: font({ Rampart }) },
     ],
   },
 ];
