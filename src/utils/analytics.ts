9731cded303a47f237a142c697bccdadddb4e264
zk-phi <zk-phi@users.noreply.github.com>
2021-07-16 01:01:23 +0900

    Fix linter issues

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,4 +1,4 @@
-// @ts-nocheck
+// @ts-nocheck : ga.js does not pass the ts typechecker
 
 /* eslint-disable */
 (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -7,9 +7,9 @@ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
 })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 /* eslint-enable */
 
-ga('create', 'UA-121793995-1', 'auto');
-
 declare const ga: (arg1: string, arg2: string, arg3: string, arg4?: string) => void;
+
+ga("create", "UA-121793995-1", "auto");
 
 export default (arg1: string, arg2: string, arg3: string, arg4?: string): void => {
   ga(arg1, arg2, arg3, arg4);
