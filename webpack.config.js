1d7c5fedb04e29638c7b15cb49166c0153bfb04b
zk-phi <zk-phi@users.noreply.github.com>
2021-07-16 21:06:43 +0900

    Fix linter issues

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = (env, argv) => ({
       }, {
         test: /\.(woff|svg|png)$/,
         type: "asset/resource",
-      },{
+      }, {
         test: /\.glsl$/,
         loader: "webpack-glsl-minify",
         options: {
