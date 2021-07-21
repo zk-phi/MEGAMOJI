002e18288a37ef53010e4f0f4ccface18f4a4a8c
zk-phi <zk-phi@users.noreply.github.com>
2021-07-21 12:45:57 +0900

    Make quantize fn do not depend on map

--- src/shaders/utils/quantize.glsl
+++ src/shaders/utils/quantize.glsl
@@ -1,3 +1,3 @@
 float quantize(float val, float from, float to, float count) {
-    return map(floor(map(val, from, to, 0., 1.) * count) / count, 0., 1., from, to);
+    return floor((val - from) / (to - from) * count) / count * (to - from) + from;
 }
