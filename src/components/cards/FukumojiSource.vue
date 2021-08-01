d6a7093932ef44f7e5dc2bd3156542be5eba81c4
zk-phi <zk-phi@users.noreply.github.com>
2021-08-02 01:23:51 +0900

    Remove redundant component

--- src/components/cards/FukumojiSource.vue
+++ src/components/cards/FukumojiSource.vue
@@ -1,7 +1,7 @@
 <script lang="ts">
 import { defineComponent } from "vue";
-import { NCard, NTabs, NTabPane } from "naive-ui";
-import PartSelect from "../formblocks/PartSelect.vue";
+import { NCard, NTabs, NTabPane, NSpace } from "naive-ui";
+import ToggleButton from "../inputs/ToggleButton.vue";
 import { EMOJI_SIZE } from "../../constants/emoji";
 import { mergeImages, urlToImg } from "../../utils/canvas";
 import * as parts from "../../constants/parts";
@@ -9,7 +9,7 @@ import empty from "../../parts/void.svg";
 
 export default defineComponent({
   components: {
-    PartSelect, NCard, NTabs, NTabPane,
+    ToggleButton, NCard, NTabs, NTabPane, NSpace,
   },
   props: {
     show: { type: Boolean, required: true },
@@ -61,11 +61,23 @@ export default defineComponent({
           v-for="category in parts.categories"
           :key="category.label"
           :name="category.label">
-        <PartSelect
-            :key="category.value"
-            v-model="conf[category.value]"
-            :parts="parts[category.value]" />
+        <NSpace>
+          <ToggleButton
+              v-for="p in parts[category.value]"
+              :key="p"
+              v-model="conf[category.value]"
+              size="part"
+              :value="p">
+            <img class="img" :src="p">
+          </ToggleButton>
+        </NSpace>
       </NTabPane>
     </NTabs>
   </NCard>
 </template>
+
+<style scoped>
+.img {
+  height: 1em;
+}
+</style>
