88893c580faffdaccd348a30b0e77c7438586f2e
zk-phi <zk-phi@users.noreply.github.com>
2021-08-17 04:02:49 +0900

    Fix fontcolorselectblock story

--- src/components/formblocks/FontColorSelectBlock.stories.js
+++ src/components/formblocks/FontColorSelectBlock.stories.js
@@ -8,11 +8,16 @@ export default {
 const Template = (args) => ({
   components: { FontColorSelectBlock },
   data: () => args,
-  template: "<FontColorSelectBlock :model-value='modelValue' :show-details='showDetails' />",
+  template: `
+    <FontColorSelectBlock
+      :model-value="modelValue"
+      :gradient="gradient"
+      :show-details="showDetails" />
+  `,
 });
 
 export const Basic = Template.bind({});
-Basic.args = { modelValue: "#ff0000", showDetails: false };
+Basic.args = { modelValue: "#ff0000", gradient: [], showDetails: false };
 
 export const Details = Template.bind({});
-Details.args = { modelValue: "#ff0000", showDetails: true };
+Details.args = { modelValue: "#ff0000", gradient: [], showDetails: true };
