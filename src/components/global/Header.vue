c8006b7a277b49f1fec55ebff35ac49046b6e47b
zk-phi <zk-phi@users.noreply.github.com>
2021-09-17 03:15:03 +0900

    Neko

--- src/components/global/Header.vue
+++ src/components/global/Header.vue
@@ -20,7 +20,7 @@ export default defineComponent({
       MEGAMOJI
     </h1>
     <p class="description">
-      カスタム絵文字をサクッと作れる
+      カスタム絵文字をサクッと作れる🐱
       <Button type="text" @click="openNews">
         🎉更新履歴
       </Button>
