9cbd05f2bba077421d75a4d3db74200ad3824afe
zk-phi <zk-phi@users.noreply.github.com>
2022-01-18 15:40:59 +0900

    Donation link

--- src/components/global/Footer.vue
+++ src/components/global/Footer.vue
@@ -10,6 +10,9 @@ export default defineComponent({
     backstage() {
       window.open("https://scrapbox.io/MEGAMOJI/%F0%9F%93%8C_%E3%81%93%E3%82%8C%E3%81%AF%E3%81%AA%E3%81%AB%EF%BC%9F");
     },
+    donation() {
+      window.open("https://note.com/zk_phi/n/n3a199c99d6d6");
+    },
   },
 });
 </script>
@@ -17,13 +20,17 @@ export default defineComponent({
 <template>
   <footer class="footer">
     <p class="p">
-      このページでは Cookie (アクセス数などの集計に Google Analytics、エラーの検知に Rollbar) を使用しています。
-    </p>
-    <p class="p">
       Built with ♡ by zk-phi
+      <Button type="text" @click="donation">
+        💝投げ銭箱
+      </Button>
+      /
       <Button type="text" @click="backstage">
         🛠開発室
       </Button>
+    </p>
+    <p class="p">
+      このページでは Cookie (アクセス数などの集計に Google Analytics、エラーの検知に Rollbar) を使用しています。
     </p>
   </footer>
 </template>
