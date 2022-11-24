788323059470b0af10206318595c4698f840a430
zk-phi <zk-phi@users.noreply.github.com>
2022-11-25 02:38:04 +0900

    Add test to generate an image

--- tests/e2e/index.test.ts
+++ tests/e2e/index.test.ts
@@ -1,9 +1,17 @@
-describe("Load index page", () => {
+const sleep = async (delay: number) => new Promise(resolve => setTimeout(resolve, delay));
+
+describe("/", () => {
   beforeAll(async () => {
     await page.goto("http://localhost:8080");
   });
-  it("Succeed to render the page", async () => {
+  it("ページが表示される", async () => {
     const text = await page.evaluate(() => document.body.textContent);
     await expect(text).toContain("MEGAMOJI");
+  });
+  it("textarea に文字列を入力すると何らかの画像が生成される", async () => {
+    await page.type("textarea", "hogen\nhoge");
+    await sleep(3000);
+    const imageSrc = await page.evaluate(() => document.body.getElementsByTagName("img")[0].src);
+    await expect(imageSrc).toContain("blob:");
   });
 });
