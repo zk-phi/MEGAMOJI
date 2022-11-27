3e3a2c2b8287eec844e09a7562dcaf316a3ca5cb
zk-phi <zk-phi@users.noreply.github.com>
2022-11-27 17:22:16 +0900

    Add tests

--- tests/e2e/index.spec.ts
+++ tests/e2e/index.spec.ts
@@ -4,15 +4,29 @@ const sleep = async (delay: number) => new Promise((resolve) => {
   setTimeout(resolve, delay);
 });
 
-test("ページが表示される", async ({ page }) => {
+test("ページタイトルが表示されている", async ({ page }) => {
   await page.goto("/");
   await expect(await page.getByRole("heading", { level: 1 })).toHaveText(/MEGAMOJI/);
 });
 
-test("textarea に文字列を入力すると何らかの画像が生成される", async ({ page }) => {
+test("ファーストビューに textarea が存在し、フォーカスが当たっている", async ({ page }) => {
   await page.goto("/");
-  await page.getByRole("textbox").fill("hoge\nhoge");
-  await sleep(3000);
-  const src = await page.evaluate(() => document.body.getElementsByTagName("img")[0].src);
+  await expect(page.locator("textarea")).toBeFocused();
+});
+
+test("textarea に文字列を入力すると何らかの画像が表示される", async ({ page }) => {
+  await page.goto("/");
+  await page.locator("textarea").fill("hoge\nhoge");
+  const src = await page.locator("img").evaluate((el) => (el as HTMLImageElement).src);
   expect(src).toMatch(/^blob:/);
+});
+
+test("保存ボタンを押すと何らかの PNG ファイルがダウンロードされる", async ({ page }) => {
+  await page.goto("/");
+  await page.locator("textarea").fill("hoge\nhoge");
+  const [ download ] = await Promise.all([
+    page.waitForEvent("download"),
+    page.locator("button", { hasText: "保存" }).click(),
+  ]);
+  expect(download.suggestedFilename()).toMatch(/\.png$/);
 });
