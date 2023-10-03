3126ff45421be30ac5a5876c304b561a701ea86e
zk-phi <zk-phi@users.noreply.github.com>
2023-10-04 00:26:11 +0900

    Make effects render meaningful content for the first frame

--- src/effects/straight.ts
+++ src/effects/straight.ts
@@ -2,7 +2,14 @@ import { Effect } from "../types";
 import { scaleCentered } from "../utils/canvas";
 
 const effectStraight: Effect = (keyframe, ctx, cellWidth, cellHeight) => {
-  scaleCentered(ctx, cellWidth, cellHeight, keyframe, keyframe);
+  const scale = keyframe < 0.125 ? (
+    1
+  ) : keyframe < 0.875 ? (
+    (keyframe - 0.125) / 0.75 // 0 -> 1
+  ) : (
+    1
+  );
+  scaleCentered(ctx, cellWidth, cellHeight, scale, scale);
 };
 
 export default effectStraight;
