33b4e16bbf62ab00d97c8c84de8ad41f84c78a98
zk-phi <zk-phi@users.noreply.github.com>
2023-10-04 11:18:09 +0900

    Add stop easing for debugging

--- src/constants/easings.ts
+++ src/constants/easings.ts
@@ -2,10 +2,16 @@ import easingLinear from "../easings/linear";
 import easingStopNGo from "../easings/stopngo";
 import easingExponent from "../easings/exponent";
 import easingBounce from "../easings/bounce";
+import easingStop from "../easings/stop";
+
+import { NODE_ENV } from "../utils/env";
 
 export default [
   { label: "スルスル", value: easingLinear },
   { label: "シュッシュ", value: easingStopNGo },
   { label: "ニュルニュル", value: easingExponent },
   { label: "ボテボテ", value: easingBounce },
+  ...(NODE_ENV === "development" ? [
+    { label: "１フレーム目固定 (デバッグ用)", value: easingStop }
+  ] : []),
 ];
