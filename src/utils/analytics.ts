6b31414bf4fd01383912add58724faa7db79c783
zk-phi <zk-phi@users.noreply.github.com>
2023-10-06 00:41:15 +0900

    Remove deprecated ga

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -5,7 +5,6 @@ let mode = "";
 const switchMode = (value: string, interactive?: boolean) => {
   mode = value;
   if (GA4_TOKEN && interactive) {
-    ga("send", "pageview", `/${value}`);
     gtag("event", "switch_mode", { mode: value });
   } else {
     // eslint-disable-next-line no-console
@@ -14,15 +13,12 @@ const switchMode = (value: string, interactive?: boolean) => {
 };
 
 const changeFont = (value: string) => {
-  ga("set", "dimension1", value);
 };
 
 const changeAnimation = (animationName: string, effectNames: string[]) => {
-  ga("set", "dimension2", `${animationName}/${effectNames.join(",")}`);
 };
 
 const render = () => {
-  ga("send", "event", mode, "render");
   if (GA4_TOKEN) {
     gtag("event", "render_emoji", { mode });
   } else {
@@ -32,7 +28,6 @@ const render = () => {
 };
 
 const download = () => {
-  ga("send", "event", mode, "download");
   if (GA4_TOKEN) {
     gtag("event", "download", { mode });
   } else {
@@ -43,8 +38,6 @@ const download = () => {
 
 export default (() => {
   if (GA4_TOKEN) {
-    ga("create", "UA-121793995-1", "auto");
-    ga("send", "pageview", "/");
     gtag("js", new Date());
     gtag("config", GA4_TOKEN);
     // eslint-disable-next-line no-console
