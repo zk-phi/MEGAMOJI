88758a18bdee6d5bc5b3817ba00c1c2dd526f3b8
Fine Archs <cicada1003@gmail.com>
2023-12-18 00:33:07 +0900

    add norinori effect

--- src/effects/norinori.ts
+++ src/effects/norinori.ts
@@ -4,11 +4,19 @@ const effectNorinori: Effect = (keyframe, ctx, cellWidth, cellHeight) => {
   const k = 1 / 4;
   const a = (2 * k - 1) / (k * k - k);
   const b = (1 - 2 * k * k) / (k * k - k);
-  const ratio = (keyframe < k
-    ? (-a * keyframe ^ 2 + -b * keyframe)
-    : (a * keyframe ^ 2 + b * keyframe + 2)
-  ) / 4;
-  ctx.transform(1 + ratio, 0, 0, 1 - ratio, -ratio * cellWidth / 2, ratio * cellHeight * 3 / 4);
+
+  const kf = (keyframe % 0.5) * 2;
+  const sign = (keyframe < 0.5) ? -1 : 1;
+  const ratio = (kf < k
+    ? (-a * kf ** 2 + -b * kf)
+    : (a * kf ** 2 + b * kf + 2)
+  ) / 6;
+
+  ctx.transform(
+    1, 0,
+    sign * ratio, 1 - ratio,
+    -sign * ratio * cellWidth * 3 / 4, ratio * cellHeight * 3 / 4
+  );
 };
 
 export default effectNorinori;
