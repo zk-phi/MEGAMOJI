8bc81910fc9e53c2523fe97c144693b3a640ecce
hata6502 <7702653+hata6502@users.noreply.github.com>
2024-04-06 14:46:11 +0900

    yurayura

--- src/effects/yurayura.ts
+++ src/effects/yurayura.ts
@@ -2,7 +2,7 @@ import { Effect } from "../types";
 
 const effectYurayura: Effect = (keyframe, ctx, cellWidth, cellHeight) => {
   ctx.translate(cellWidth / 2, cellHeight * 3 / 4);
-  ctx.rotate(0.25 * Math.sin(keyframe * 2 * Math.PI + 0.1));
+  ctx.rotate(0.25 * Math.sin(keyframe * 2 * Math.PI + 0.25));
   ctx.translate(-cellWidth / 2, -cellHeight * 3 / 4);
 };
 
