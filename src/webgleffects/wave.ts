14caf2775c7970f508be6a72287104deaec2e58c
zk-phi <zk-phi@users.noreply.github.com>
2026-05-10 22:18:59 +0900

    Tweak wave effect strength

--- src/webgleffects/wave.ts
+++ src/webgleffects/wave.ts
@@ -7,7 +7,7 @@ const shader = webglEffectShader(shaderWave.sourceCode);
 const webglWave: WebGLEffect = (keyframe) => {
   const program = webglLoadEffectShader(shader);
   webglSetFloat(program, "frequency", 2.0);
-  webglSetFloat(program, "amplitude", 0.04);
+  webglSetFloat(program, "amplitude", 0.02);
   webglSetFloat(program, "keyframe", keyframe);
   return program;
 };
