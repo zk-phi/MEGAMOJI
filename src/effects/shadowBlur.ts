833153aee34ca60c6677ab82377ffa047ae714c1
zk-phi <zk-phi@users.noreply.github.com>
2026-05-26 21:06:43 +0900

    Fix shadowBlur effect

--- src/effects/shadowBlur.ts
+++ src/effects/shadowBlur.ts
@@ -1,9 +1,7 @@
 import { Effect } from "../types";
-import { HSV2RGB } from "../utils/color";
 
 const effectShadowBlur: Effect = (keyframe, ctx) => {
-  const HSVColor = HSV2RGB({ h: 0, s: 0, v: keyframe });
-  ctx.shadowColor = `rgb(${HSVColor.r}, ${HSVColor.g}, ${HSVColor.b})`;
+  ctx.shadowColor = `rgba(0, 0, 0, ${1 - keyframe})`;
   ctx.shadowBlur = 50 * keyframe;
 };
 
